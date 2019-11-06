7605ebadee1166fba3a96fd1bcc1afa1073ff7ed
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2019-11-06 16:24:26 +0530

    Flutter - Stateful and Stateless Widgets

---
+++ Flutter - Stateful and Stateless Widgets/make_it_rain/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:make_it_rain/ui/make_it_rain.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: "Make It Rain",
+    home: new MakeItRain(),
+  ));
+}
