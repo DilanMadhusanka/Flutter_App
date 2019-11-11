d1b7bcadb0f685349e4e2e331e4db42d9dcd2a70
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2019-11-11 13:16:09 +0530

    Add the logic to the app

--- 6. Flutter - Build The Weight On Planet X App/weightonplanet/lib/ui/home.dart
+++ 6. Flutter - Build The Weight On Planet X App/weightonplanet/lib/ui/home.dart
@@ -4,18 +4,34 @@ class Home extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return new HomeState();
-      }
-    
-    }
-    
+  }
+}
+
 class HomeState extends State<Home> {
+  final TextEditingController _weightController = new TextEditingController();
   int radioValue = 0;
+  double _finalResult = 0.0;
+  String _formattedText = "";
   void handleRadioValueChange(int value) {
     setState(() {
       radioValue = value;
-      print(radioValue);
+      // print(radioValue);
+      switch (radioValue) {
+        case 0:
+          _finalResult = calculateWeight(_weightController.text, 0.06);
+          _formattedText = "Your weight on Pluto is ${_finalResult.toStringAsFixed(1)}";
+          break;
+        case 1:
+          _finalResult = calculateWeight(_weightController.text, 0.38);
+          _formattedText = "Your weight on Mars is ${_finalResult.toStringAsFixed(1)}";
+          break;
+        case 2:
+          _finalResult = calculateWeight(_weightController.text, 0.91);
+          _formattedText = "Your weight on Venus is ${_finalResult.toStringAsFixed(1)}";
+      }
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -30,7 +46,8 @@ class HomeState extends State<Home> {
         child: new ListView(
           padding: const EdgeInsets.all(2.5),
           children: <Widget>[
-            new Image.asset("images/planet.png", 
+            new Image.asset(
+              "images/planet.png",
               height: 133.0,
               width: 200.0,
             ),
@@ -40,41 +57,56 @@ class HomeState extends State<Home> {
               child: new Column(
                 children: <Widget>[
                   new TextField(
-                    controller: null,
+                    controller: _weightController,
                     keyboardType: TextInputType.number,
                     decoration: new InputDecoration(
-                      labelText: 'Your Weight on Earth',
-                      hintText: 'In Pounds',
-                      icon: new Icon(Icons.person_outline)
-                    ),
+                        labelText: 'Your Weight on Earth',
+                        hintText: 'In Pounds',
+                        icon: new Icon(Icons.person_outline)),
                   ),
-                  new Padding(padding: new EdgeInsets.all(5.0),),
+                  new Padding(
+                    padding: new EdgeInsets.all(5.0),
+                  ),
                   new Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                       new Radio(
                         activeColor: Colors.brown,
-                        value: 0, groupValue: radioValue, onChanged: handleRadioValueChange,),
-                      new Text("Pluto", style: new TextStyle(color: Colors.white30),),
+                        value: 0,
+                        groupValue: radioValue,
+                        onChanged: handleRadioValueChange,
+                      ),
+                      new Text(
+                        "Pluto",
+                        style: new TextStyle(color: Colors.white30),
+                      ),
                       new Radio(
                         activeColor: Colors.red,
-                        value: 1, groupValue: radioValue, onChanged: handleRadioValueChange,),
-                      new Text("Mars", style: new TextStyle(color: Colors.white30)),
+                        value: 1,
+                        groupValue: radioValue,
+                        onChanged: handleRadioValueChange,
+                      ),
+                      new Text("Mars",
+                          style: new TextStyle(color: Colors.white30)),
                       new Radio(
                         activeColor: Colors.orangeAccent,
-                        value: 2, groupValue: radioValue, onChanged: handleRadioValueChange,),
-                      new Text("Venus", style: new TextStyle(color: Colors.white30))
+                        value: 2,
+                        groupValue: radioValue,
+                        onChanged: handleRadioValueChange,
+                      ),
+                      new Text("Venus",
+                          style: new TextStyle(color: Colors.white30))
                     ],
                   ),
-                  new Padding(padding: new EdgeInsets.all(15.6),),
+                  new Padding(
+                    padding: new EdgeInsets.all(15.6),
+                  ),
                   new Text(
-                    "Hello there",
-                    style: new TextStyle(
-                      color: Colors.white,
-                      fontSize: 19.4,
-                      fontWeight: FontWeight.w500
-                    )
-                  )
+                      _weightController.text.isEmpty ? "Please enter weight" : _formattedText + "lbs",
+                      style: new TextStyle(
+                          color: Colors.white,
+                          fontSize: 19.4,
+                          fontWeight: FontWeight.w500))
                 ],
               ),
             )
@@ -82,5 +114,14 @@ class HomeState extends State<Home> {
         ),
       ),
     );
+  }
+
+  double calculateWeight(String weight, double multiplier) {
+    if (weight.isNotEmpty && int.parse(weight) > 0)
+      return int.parse(weight) * multiplier;
+    else {
+      print("Wrong!");
+      return int.parse("180") * 0.38;
+    }
   }
 }
