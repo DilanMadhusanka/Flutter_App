4314facee11d6ba7f5e2959008e2933e9e321fd8
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2019-11-17 21:41:01 +0530

    Quake App- Fixing Return Types - Getting JSON Data

---
+++ 10. Challenge Solution - Build a Quake App/quakes/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+void main() async {
+  Map _data = await getQuakes();
+  print(_data['features'][0]['properties']);
+  runApp(new MaterialApp(
+    title: 'Quakes',
+    home: new Quakes(),
+  ));
+}
+
+class Quakes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Quakes'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+
+}
+
+Future<Map> getQuakes() async {
+  String apiUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
+  http.Response response = await http.get(apiUrl);
+  return json.decode(response.body);
+}
