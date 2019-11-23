16f12359d9c95a83d52c271bf4ddbb76d1f6c577
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2019-11-23 21:16:32 +0530

    create insert method

--- 13. Flutter - Database Using SQFLITE/database_intro/lib/utils/database_helper.dart
+++ 13. Flutter - Database Using SQFLITE/database_intro/lib/utils/database_helper.dart
@@ -1,9 +1,19 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 class DatabaseHelper {
   static final DatabaseHelper _instance = new DatabaseHelper.internal();
   
   factory DatabaseHelper() => _instance;
+
+  final String tableUser = "userTable";
+  final String columnId = "id";
+  final String columnUserName = "userName";
+  final String columnPassword = "password";
 
   static Database _db;
   Future<Database> get db async {
@@ -15,8 +25,16 @@ class DatabaseHelper {
   }
 
   DatabaseHelper.internal();
-  
-  initDb() async {
 
+  initDb() async {
+    Directory documentDerectory = await getApplicationDocumentsDirectory();
+    String path = join(documentDerectory.path, "maindb.db");
+    var ourDb = await openDatabase(path, version: 1, onCreate:_onCreate)
+  }
+
+  void _onCreate(Database db, int version) async {
+    await db.execute(
+      "CREATE TABLE $tableUser($columnId INTEGER PRIMARY KEY, $columnUserName TEXT, $columnPassword TEXT)"
+    );
   }
 }
