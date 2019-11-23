cf5a6f784e738dffab481d52ae4ed356338af40b
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2019-11-23 06:12:23 +0530

    Creating Database Client Helper Class Structurer

--- 13. Flutter - Database Using SQFLITE/database_intro/lib/utils/database_helper.dart
+++ 13. Flutter - Database Using SQFLITE/database_intro/lib/utils/database_helper.dart
@@ -1,7 +1,22 @@
-class DatabaseHelper {
-  static final DatabaseHelper _instance = new DatabaseHelper().internal();
-  
-    factory DatabaseHelper() => _instance;
+import 'package:sqflite/sqflite.dart';
 
-    DatabaseHelper.internal();
+class DatabaseHelper {
+  static final DatabaseHelper _instance = new DatabaseHelper.internal();
+  
+  factory DatabaseHelper() => _instance;
+
+  static Database _db;
+  Future<Database> get db async {
+    if(_db != null) {
+      return _db;
+    }
+    _db = await initDb();
+    return _db;
+  }
+
+  DatabaseHelper.internal();
+  
+  initDb() async {
+
+  }
 }
